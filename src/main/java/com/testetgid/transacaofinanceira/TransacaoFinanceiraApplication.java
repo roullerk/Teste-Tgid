25b6ec16a906e8b77175ca64d823ff1cd501ecf3
roullerk <victorfrds@gmail.com>
2024-01-20 22:35:09 -0300

    Criação do projeto

---
+++ src/main/java/com/testetgid/transacaofinanceira/TransacaoFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.testetgid.transacaofinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoFinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testetgid/transacaofinanceira/TransacaoFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testetgid.transacaofinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
